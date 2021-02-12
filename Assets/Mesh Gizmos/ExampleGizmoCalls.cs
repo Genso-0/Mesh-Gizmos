e03fa6cdd7472bd373be17abe2600703c9eb7364
Genso <anastazio@live.co.uk>
2021-02-12 18:48:52 +0100

    Some file renaming.

--- Assets/Mesh Gizmos/ExampleGizmoCalls.cs
+++ Assets/Mesh Gizmos/ExampleGizmoCalls.cs
@@ -18,10 +18,10 @@ public class ExampleGizmoCalls : MonoBehaviour
         _Gizmos.DrawRay(transform.position - transform.right, transform.forward, Color.green);
         _Gizmos.DrawCube(transform.position + position_cubeGizmo, Vector3.zero, Color.red);
         _Gizmos.DrawSphere(transform.position + position_sphereGizmo, Vector3.zero, Color.green);
-        _Gizmos.DrawArrowHead(transform.position + position_arrowGizmo, Vector3.zero, Color.magenta);
-        
+        _Gizmos.DrawArrowHead(transform.position + position_arrowGizmo, Vector3.zero, Color.magenta); 
+
         //Combine Gizmos
-        var arrowStart = transform.position - transform.right + (transform.forward * 1.5f);
+       var arrowStart = transform.position - transform.right + (transform.forward * 1.5f);
         var arrowDirection = transform.right;
         _Gizmos.DrawRay(arrowStart, arrowDirection, Color.magenta);
         _Gizmos.DrawArrowHead(arrowStart + arrowDirection, arrowDirection, Color.white);
