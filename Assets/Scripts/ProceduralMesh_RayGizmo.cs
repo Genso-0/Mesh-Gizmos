39821546ea540a76b7b9625085478e263184af27
Genso <anastazio@live.co.uk>
2021-02-13 13:44:26 +0100

    Refactoring: ProceduralMesh_RayGizmo.cs

--- Assets/Scripts/ProceduralMesh_RayGizmo.cs
+++ Assets/Scripts/ProceduralMesh_RayGizmo.cs
@@ -9,9 +9,9 @@ namespace Mesh_Gizmos
     public class ProceduralMesh_RayGizmo : MonoBehaviour
     { 
         [HideInInspector] MeshFilter meshFilter;
-        List<Vector3> vertices;
-        int[] triangleIndices;
-        Vector2[] uv;
+        List<Vector3> verts;
+        int[] tris;
+        Vector2[] uvs;
         readonly int resolution = 20;  
         [HideInInspector] public Material material;
         void Awake()
@@ -21,9 +21,9 @@ namespace Mesh_Gizmos
             GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
             var vertCount = (resolution + 2) * 2;
             int indexCount = (((resolution * 3) * 2) + (resolution * 6));
-            vertices = new List<Vector3>(vertCount);
-            triangleIndices = new int[indexCount];
-            uv = new Vector2[vertCount];
+            verts = new List<Vector3>(vertCount);
+            tris = new int[indexCount];
+            uvs = new Vector2[vertCount];
         }
         public void Draw(Vector3 start, Vector3 end, Color32 color, float radius)
         {
@@ -32,11 +32,11 @@ namespace Mesh_Gizmos
         }
         public void BuildMesh_Cylinder(Vector3 start, Vector3 end, float radius)
         {
-            vertices.Clear();
+            verts.Clear();
             int triIndex = 0;
             BuildCylinderDiscs(start, end, radius, ref triIndex);
             ConnectDiscs(triIndex);
-            SetMesh(vertices, triangleIndices, uv);
+            SetMesh();
         }
         public void BuildCylinderDiscs(Vector3 start, Vector3 end, float radius, ref int triIndex)
         {
@@ -48,13 +48,13 @@ namespace Mesh_Gizmos
         } 
         void BuildDisc(Vector3 localRight, Vector3 localUp, Vector3 offset, float angle, float radius, float segmentWidth, ref int triIndex, bool fliped = false)
         {
-            var vertCount = vertices.Count;
+            var vertCount = verts.Count;
             int tri1 = fliped ? 2 : 0;
             int tri2 = 1;
             int tri3 = fliped ? 0 : 2;
             int jCount = fliped ? vertCount - 2 : 0;
-            uv[vertCount] = new Vector2(offset.x / radius, offset.y / radius);
-            vertices.Add(offset);
+            uvs[vertCount] = new Vector2(offset.x / radius, offset.y / radius);
+            verts.Add(offset);
             SetDiscVertex(localRight, localUp, offset, segmentWidth, radius, ref angle);
 
             for (int i = 2; i < resolution + 2; ++i)
@@ -62,9 +62,9 @@ namespace Mesh_Gizmos
                 SetDiscVertex(localRight, localUp, offset, segmentWidth, radius, ref angle);
 
                 var j = ((i - 2) + jCount) * 3;
-                triangleIndices[j + tri1] = 0 + vertCount;
-                triangleIndices[j + tri2] = i - 1 + vertCount;
-                triangleIndices[j + tri3] = i + vertCount;
+                tris[j + tri1] = 0 + vertCount;
+                tris[j + tri2] = i - 1 + vertCount;
+                tris[j + tri3] = i + vertCount;
                 triIndex += 3;
             }
         }
@@ -75,8 +75,8 @@ namespace Mesh_Gizmos
 
             var point = x * axisA + y * axisB;
             point += offset;
-            uv[vertices.Count] = new Vector2(x, y);
-            vertices.Add(point);
+            uvs[verts.Count] = new Vector2(x, y);
+            verts.Add(point);
             angle += segmentWidth;
         }
         void GetLocalAxis(Vector3 forceDirection, out Vector3 localRight, out Vector3 localUp)
@@ -88,22 +88,22 @@ namespace Mesh_Gizmos
         {
             for (int i = 0; i < resolution; i++)
             {
-                triangleIndices[triIndex] = i + 1;
-                triangleIndices[triIndex + 1] = i + 2 + resolution + 1;
-                triangleIndices[triIndex + 2] = i + 3 + resolution + 1;
+                tris[triIndex] = i + 1;
+                tris[triIndex + 1] = i + 2 + resolution + 1;
+                tris[triIndex + 2] = i + 3 + resolution + 1;
 
-                triangleIndices[triIndex + 3] = i + 2;
-                triangleIndices[triIndex + 4] = i + 1;
-                triangleIndices[triIndex + 5] = i + 3 + resolution + 1;
+                tris[triIndex + 3] = i + 2;
+                tris[triIndex + 4] = i + 1;
+                tris[triIndex + 5] = i + 3 + resolution + 1;
                 triIndex += 6;
             }
         }
-        internal void SetMesh(List<Vector3> vertices, int[] tris, Vector2[] uv)
+        internal void SetMesh()
         {
             meshFilter.mesh.Clear();
-            meshFilter.mesh.SetVertices(vertices);
+            meshFilter.mesh.SetVertices(verts);
             meshFilter.mesh.SetIndices(tris, MeshTopology.Triangles, 0);
-            meshFilter.mesh.uv = uv;
+            meshFilter.mesh.uv = uvs;
             meshFilter.mesh.RecalculateNormals();
             meshFilter.mesh.RecalculateBounds();
         } 
